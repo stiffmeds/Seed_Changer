cb1f90511a75b64d023c9c67dad1166aa39e8907
stiffmeds <6158160+stiffmeds@users.noreply.github.com>
2023-09-05 11:04:50 +0800

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Logging;
+using UnityEngine.InputSystem;
+
+namespace Seed_Changer
+{
+    [BepInPlugin("com.meds.seedchanger", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new("com.meds.seedchanger");
+        internal static ManualLogSource Log;
+        private void Awake()
+        {
+            // Plugin startup logic
+            Log = Logger;
+            harmony.PatchAll();
+            Logger.LogInfo($"Seed Changer has loaded!");
+        }
+    }
+    [HarmonyPatch]
+    public class SeedChanger
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(InputController), "DoKeyBinding")]
+        public static bool DoKeyBindingPrefix(ref InputAction.CallbackContext _context)
+        {
+            if ((UnityEngine.Object)MapManager.Instance != (UnityEngine.Object)null && Keyboard.current != null && _context.control == Keyboard.current[Key.F2])
+            {
+                AlertManager.buttonClickDelegate = new AlertManager.OnButtonClickDelegate(ActuallyChangeSeed);
+                AlertManager.Instance.AlertInput(Texts.Instance.GetText("gameSeedInput"), Texts.Instance.GetText("accept").ToUpper());
+                return false;
+            }
+            return true;
+        }
+        public static void ActuallyChangeSeed()
+        {
+            AlertManager.buttonClickDelegate -= new AlertManager.OnButtonClickDelegate(ActuallyChangeSeed);
+            if (AlertManager.Instance.GetInputValue() == null)
+                return;
+            string upper = AlertManager.Instance.GetInputValue().ToUpper();
+            if (!(upper.Trim() != ""))
+                return;
+            AtOManager.Instance.SetGameId(upper);
+            AtOManager.Instance.SaveGame();
+            OptionsManager.Instance.DoExit();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Seed Changer
+
+Dev tool that allows game seed to be changed mid-run.
+
+To use it: press F2 at the Map screen, enter your new seed and then reload the save.
+
+Please do not use this for cheating :(
+
+## Installation
+
+Install [BepInEx](https://across-the-obelisk.thunderstore.io/package/BepInEx/BepInExPack_AcrossTheObelisk/).
+
+Download the [latest release](https://github.com/stiffmeds/Seed_Changer) and put it in your BepInEx _plugins_ folder.
+
+## Support
+
+Raise a Github issue or ping @stiffmeds in the #modding channel of the [official Across the Obelisk Discord](https://discord.gg/across-the-obelisk-679706811108163701).
+
+## Donations
+
+I make mods because I love it, not because I want to make money from it. If you really, really want to donate to me, my preferred non-profit organisations are [Greyhound Adoptions WA](https://greyhoundadoptionswa.com.au/donation/) ([paypal](https://www.paypal.com/donate?token=m8DwEGGEH0FFsS6PS-5p4MX9_5g8_ocMMrNFjaELN-xcG6Ok-KCFabu5xtB-57QBiOM7QLSuKVUepvL_)) or [Headache Australia](https://headacheaustralia.org.au/donate/).
